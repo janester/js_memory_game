ef31bc0f16db1f0bb1913d811f326d0e3e1795ca
Jane Sternbach <janesternbach@gmail.com>
2013-07-01 13:24:33 -0700

    change class on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  console.log(':)')
-})
+  var tiles = $(".tile");
+  var num_clicks = 0;
+  $("#board").on("click", ".tile", foo);
+});
+
+
+function foo() {
+  $(this).addClass("active")
+}
